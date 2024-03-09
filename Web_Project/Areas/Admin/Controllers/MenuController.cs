0da974943d13eddbfbc2daed7997643e6573fea3
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2024-03-09 15:05:44 +0700

    thêm được menu giao diện người dùng,Admin

---
+++ Web_Project/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Web_Project.Models;
+
+namespace Web_Project.Areas.Admin.Controllers
+{
+    public class MenuController : Controller
+    {
+        private Restaurant_Web_DataEntities db = new Restaurant_Web_DataEntities();
+        // GET: Admin/Menu
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var list = db.Menus.OrderBy(b => b.id).ToPagedList(pageNumber, pageSize);
+            return View(list);
+        }
+
+        // GET: Admin/Menu/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Create
+        public ActionResult Create()
+        {
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name");
+            return View();
+        }
+
+        // POST: Admin/Menu/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,img,name,category_id,price,description")] Menu menu, HttpPostedFileBase img)
+        {
+            if (ModelState.IsValid)
+            {
+                if (img != null && img.ContentLength > 0)
+                {
+                    var newFileName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Theme/img/Foods"), newFileName);
+                    img.SaveAs(path);
+                    menu.img = newFileName;
+                }
+
+                // Save the rest of the model and perform other necessary actions
+                db.Menus.Add(menu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", menu.category_id);
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", menu.category_id);
+            return View(menu);
+        }
+
+        // POST: Admin/Menu/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,img,name,category_id,price,description")] Menu menu, HttpPostedFileBase img)
+        {
+            if (ModelState.IsValid)
+            {
+                // Handle file upload
+                if (img != null && img.ContentLength > 0)
+                {
+                    var newFileName = Guid.NewGuid().ToString() + Path.GetExtension(img.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Theme/img/Foods"), newFileName);
+                    img.SaveAs(path);
+                    menu.img = newFileName;
+                }
+
+                db.Entry(menu).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", menu.category_id);
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = db.Menus.Find(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Admin/Menu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Menu menu = db.Menus.Find(id);
+            db.Menus.Remove(menu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Web_Project/Controllers/RestaurantController.cs
+++ Web_Project/Controllers/RestaurantController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_Project.Models;
 
 namespace Web_Project.Controllers
 {
     public class RestaurantController : Controller
     {
+        private Restaurant_Web_DataEntities db = new Restaurant_Web_DataEntities();
         // GET: Restaurant
         public ActionResult Index()
         {
@@ -19,6 +21,11 @@ namespace Web_Project.Controllers
         }
         public ActionResult Contact()
         {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            var menulist = db.Menus.ToList();
             return View();
         }
         public ActionResult Deals()
